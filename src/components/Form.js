ea040ccea0ee5b43eaac8682b4869a2aebccf070
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2021-11-09 20:54:18 -0500

    finished

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,18 +3,50 @@ import React from "react";
 
 
 const Form = (props) => {
+    
+    const handleChange = evt => {
+        const { name, value } = evt.target;
+        props.change(name, value);
+    }
+
+    const handleSubmit = evt => {
+        evt.preventDefault();
+        props.submit();
+    }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label> Name
                 <input 
                     type='text'
                     placeholder='Enter name'
-                    value={values.name}
-                    onChange={onChange}
+                    value={props.values.name}
+                    name='name'
+                    onChange={handleChange}
                     maxLength='30'
                 />
             </label>
+
+            <label> Email
+                <input
+                    type='email'
+                    placeholder='Enter valid email address'
+                    value={props.values.email}
+                    name='email'
+                    onChange={handleChange}
+                />
+            </label>
+
+            <label> Role
+                <input 
+                    type='text'
+                    placeholder='Enter role here'
+                    value={props.values.role}
+                    name='role'
+                    onChange={handleChange}
+                />
+            </label>
+            <input type='submit' value='Create your team' />
         </form>
     )
 }
